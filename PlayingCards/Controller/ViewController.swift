fd2ec52f206d95c65ae019f982507f8a6a0e9154
Henrik <henrikaos91@gmail.com>
2018-04-19 14:20:05 +0200

    Initial Commit

---
+++ PlayingCards/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PlayingCards
+//
+//  Created by Henrik Anthony Odden Sandberg on 15.04.2018.
+//  Copyright © 2018 Henrik Anthony Odden Sandberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var deck = PlayingCardDeck()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
+
