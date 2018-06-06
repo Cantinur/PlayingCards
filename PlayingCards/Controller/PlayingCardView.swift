7e5eab257cbd48c809185faf3eb49411cedd62d3
Henrik <henrikaos91@gmail.com>
2018-06-07 01:57:34 +0200

    Some changes. Making this app in to a concentration game with cards

--- PlayingCards/Controller/PlayingCardView.swift
+++ PlayingCards/Controller/PlayingCardView.swift
@@ -21,6 +21,24 @@ class PlayingCardView: UIView{
     @IBInspectable
     var isFaceUp: Bool = true { didSet{setNeedsDisplay(); setNeedsLayout() } }
     
+    
+    var faceCardScale: CGFloat = SizeRatio.faceCardImageSizeToBaundsSize {
+        didSet{
+            setNeedsDisplay()
+        }
+    }
+    
+    @objc func adjustFaceCardScale(byHandlingGestureRecognizerBy recognizer: UIPinchGestureRecognizer){
+        switch recognizer.state {
+            
+        case .changed, .ended:
+            faceCardScale *= recognizer.scale
+            recognizer.scale = 1.0
+            
+        default: break
+        }
+    }
+    
     private func centeredAttributedString(_ string: String, fontSize: CGFloat) -> NSAttributedString{
         var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
         
@@ -85,7 +103,7 @@ class PlayingCardView: UIView{
         
         if isFaceUp {
             if let faceCardImage = UIImage(named: rankString+suit, in: Bundle(for: self.classForCoder), compatibleWith: traitCollection){
-                faceCardImage.draw(in: bounds.zoom(by: SizeRatio.faceCardImageSizeToBaundsSize))
+                faceCardImage.draw(in: bounds.zoom(by: faceCardScale))
             } else {
                 drawPips()
             }
